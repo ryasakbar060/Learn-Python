99447160d474c9e4ae8c819235b1ff5f16699c52
ryasakbar060 <ryasakbar060@gmail.com>
2022-09-23 00:12:22 +0800

    IF dan ELSE Statement

---
+++ 21-IfElseStatement.py
@@ -0,0 +1,28 @@
+# IF dan ELSE Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. Program if inline
+# if nama == "ucup" : print("Kamu Ganteng!!!!!!")
+# print(f"Terimakasih {nama}")
+
+# 2. Program if indentation
+# if nama == "ucup" :
+#     print("Kamu ganteng abiezzz!!!!")
+#     print("Kamu juga keren abiezz!!!")
+# print(f"Terimakasih {nama}")
+
+# 3. Else Statement
+if nama == "Otong" :
+    print("Hai otonggg, si kerennn!!!")
+else:
+    print("Ah kamu bukan otong, kamu gak kece!!!")
+print("Akhir program")
