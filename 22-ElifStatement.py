a39d860315356feb802208ec67b6e5f9c878d8d5
ryasakbar060 <ryasakbar060@gmail.com>
2022-09-25 00:06:47 +0800

    ELIF Statement

---
+++ 22-ElifStatement.py
@@ -0,0 +1,14 @@
+# ELIF Statement / else if statement
+
+nama = input("Nama kamu siapa? ")
+
+if nama == "ucup": # kondisi 1
+    print("Hai Ganteng ucup!!!") # aksi true 1
+elif nama == "otong": # kondisi 2
+    print("Hai Ganteng otong!!!") # aksi true 2
+elif nama == "gandrung": # kondisi 3
+    print("Hai Ganteng gandrung!!!") # aksi true 3
+else:
+    print("au ah gak kenallll")
+
+print("Akhir programmmmmm")
