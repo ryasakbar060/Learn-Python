8ed87a28fdeaf5332d409f2b5f6f951bdb6248e1
ryasakbar060 <ryasakbar060@gmail.com>
2022-09-25 23:39:36 +0800

    While Loop

---
+++ 25-WhileLoop.py
@@ -0,0 +1,17 @@
+# While Loop 
+
+# while kondisi:
+#     aksi ini
+#     aksi itu
+
+# akhir dari program
+
+angka = 0
+print(f"angka sekarang -> {angka}")
+
+while angka < 5:
+    angka += 1
+    print(f"angka sekarang -> {angka}")
+    print("Ryas keren gilak!!!")
+
+print("Keep Calm cuyyyy")
