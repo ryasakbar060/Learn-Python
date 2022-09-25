285cfabd29cd7496687134d7ef2f40e41dfa5a8a
ryasakbar060 <ryasakbar060@gmail.com>
2022-09-25 23:26:33 +0800

    For loop (perulangan)

---
+++ 24-ForLoop.py
@@ -0,0 +1,39 @@
+# For Loop (Perulangan)
+
+# for kondisi: 
+#     aksi
+
+# Dengan list
+angka2_list = [0,3,4,8,1] # ini adalah list
+print(angka2_list)
+
+for i in angka2_list:
+    print(f"i sekarang -> {i}")
+
+print("Akhir program 1\n")
+
+# ini dengan range
+angka2_range = range(5)
+
+for i in angka2_range:
+    print(f"i sekarang -> {i}")
+
+print("Akhir program 2\n")
+
+angka2_range = range(1,10)
+
+for i in angka2_range:
+    # print(f"i sekarang -> {i}")
+    print("Bebas Kamu mau ngapaian dan keluarin apa!!")
+
+print("Akhir program 3\n")
+
+# menggunakan string
+data_str = "saya ganteng abis"
+
+for huruf in data_str:
+    print(huruf)
+
+print("Akhir program 4\n")
+
+
