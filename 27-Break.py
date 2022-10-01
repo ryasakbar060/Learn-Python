1ac23b77d9336415f829fa44ac17f6f6086a00f8
ryasakbar060 <ryasakbar060@gmail.com>
2022-10-02 01:04:33 +0800

    Break

---
+++ 27-Break.py
@@ -0,0 +1,33 @@
+# Break
+
+# Contoh Pertama
+angka = 0
+
+while angka < 5:
+    angka += 1
+    print(f"angka sekarang -> {angka}")
+
+    if angka == 3:
+        print("nice!")
+        break
+
+    print("whtshapp!")
+
+print("finish!")
+
+data_int = int(input("Hitung sampai = "))
+
+# Contoh Kedua
+angka = 0
+
+while True:
+    angka += 1
+    print(f"count {angka}")
+
+    if angka == data_int:
+        print("nice!")
+        break
+
+    print("whtshapp!")
+
+print("finish!")
