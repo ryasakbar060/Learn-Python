e9231bc05ce94b3f32cdabd174d1b96d37f92609
ryasakbar060 <ryasakbar060@gmail.com>
2022-10-02 00:51:28 +0800

    Continue dan Pass

---
+++ 26-ContinueDanPass.py
@@ -0,0 +1,28 @@
+# Continue dan Pass
+
+# pass -> berfungsi sebagai dummy, tidak akan dieksekusi
+
+# angka = 0 
+
+# while angka < 5:
+#     angka += 1
+#     if angka == 3:
+#         pass # ini tidak akan dieksekusi
+#     print(angka) 
+
+# continue
+angka = 0
+
+print(f"angka sekarang -> {angka}")
+
+while angka < 5:
+    angka += 1
+    print(f"angka sekarang -> {angka}") # aksi 1
+
+    if angka == 3:
+        print("nice!")
+        continue # akan membuat loop meloncat ke step selanjutnya
+
+    print("whtshappp!") # aksi 2
+
+print("finish!")
